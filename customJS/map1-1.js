616ff0732801c54f4e4ade63a30bc0ce5d1ec6a6
Davo1987 <43937546+Davo1987@users.noreply.github.com>
2020-07-23 20:47:32 +0100

    update
    New lightobx gallery added to lake district. Helvellyn isnt working correctly at the minute. Maps base layer changed to outdoors-v11

--- customJS/map1-1.js
+++ customJS/map1-1.js
@@ -1,14 +1,15 @@
 var mymap = L.map('mapid').setView([54.526693, -3.017289], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var standard = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
-            id: 'mapbox/streets-v11',
+            id: 'mapbox/outdoors-v11',
             tileSize: 512,
             zoomOffset: -1,
             accessToken: 'pk.eyJ1IjoiZGF2bzE5ODciLCJhIjoiY2tjMGY1MDVnMWoxaDM0bXg1dTc0c3BicCJ9.btpO9V_p3D1_OS6cfhml2Q'
         }).addTo(mymap);
 
+        
 
 // Helvellyn marker and popup
 var helvellyn = L.marker([54.526693, -3.017289]).bindPopup('Helvellyn Summit').addTo(mymap);
